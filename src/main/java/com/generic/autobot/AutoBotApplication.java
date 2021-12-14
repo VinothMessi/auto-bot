e01f6fb701c96c1f9487a4a50589a3799e4c4833
VI20300045 <vinothone2one@gmail.com>
2021-12-14 20:52:01 +0530

    Spring Boot Project

---
+++ src/main/java/com/generic/autobot/AutoBotApplication.java
@@ -0,0 +1,13 @@
+package com.generic.autobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoBotApplication.class, args);
+	}
+
+}
