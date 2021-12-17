3a1db80f8f0302ff6dcbf375eaa959ef7d74967b
VI20300045 <vinothone2one@gmail.com>
2021-12-17 20:20:41 +0530

    Added more test cases

--- src/test/java/com/generic/autobot/testcases/FlightBooking.java
+++ src/test/java/com/generic/autobot/testcases/FlightBooking.java
@@ -2,6 +2,7 @@ package com.generic.autobot.testcases;
 
 import com.generic.autobot.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 import org.springframework.beans.factory.annotation.Value;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -12,9 +13,6 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 public class FlightBooking extends BaseTest {
-    By header = By.id("header_14");
-    By subHeader = By.xpath("(//h1)[2]");
-
     @Value("${flight.url}")
     private String flight;
 
@@ -28,13 +26,113 @@ public class FlightBooking extends BaseTest {
     public void launch_application() {
         browser.manage().window().maximize();
         browser.navigate().to(flight);
+
+        By header = By.id("header_14");
+        By subHeader = By.xpath("(//h1)[2]");
+
         assertThat(browser.findElement(header).getText().trim(), is(equalTo(heading)));
         assertThat(browser.findElement(subHeader).getText().trim(), is(equalTo(subHeading)));
     }
 
     @Test
-    public void flight_booking() {
-        System.out.println("Flight Booking");
+    public void tc_01_fill_contact_information() {
+        By firstName = By.name("firstName");
+        By lastName = By.name("lastName");
+        By phone = By.name("phone");
+        By email = By.name("userName");
+
+        browser.findElement(firstName).sendKeys("Lionel");
+        browser.findElement(lastName).sendKeys("Messi");
+        browser.findElement(phone).sendKeys("1234567890");
+        browser.findElement(email).sendKeys("Lionel.Messi@gmail.com");
+    }
+
+    @Test
+    public void tc_02_fill_mailing_information() {
+        By address = By.name("address1");
+        By city = By.name("city");
+        By state = By.name("state");
+        By postalCode = By.name("postalCode");
+        By country = By.name("country");
+
+        browser.findElement(address).sendKeys("Rosario, Argentina");
+        browser.findElement(city).sendKeys("Bruno Ares");
+        browser.findElement(state).sendKeys("Rosario");
+        browser.findElement(postalCode).sendKeys("Lionel.Messi@gmail.com");
+
+        Select fCountry = new Select(browser.findElement(country));
+        fCountry.selectByValue("ARGENTINA");
+    }
+
+    @Test
+    public void tc_03_fill_user_information() {
+        By userName = By.name("email");
+        By password = By.name("password");
+        By confirmPassword = By.name("confirmPassword");
+        By submit = By.name("register");
+
+        browser.findElement(userName).sendKeys("LionelMessi");
+        browser.findElement(password).sendKeys("lionelmessi");
+        browser.findElement(confirmPassword).sendKeys("lionelmessi");
+        browser.findElement(submit).click();
+    }
+
+    @Test
+    public void tc_04_fill_flight_details() throws InterruptedException {
+        By flights = By.id("flight-link");
+        By roundtrip = By.xpath("//input[@value='roundtrip']");
+        By oneway = By.xpath("//input[@value='oneway']");
+        By passengers = By.name("passCount");
+        By departingFrom = By.name("fromPort");
+        By arrivingIn = By.name("toPort");
+        By fromMonth = By.name("fromMonth");
+        By fromDay = By.name("fromDay");
+        By toMonth = By.name("toMonth");
+        By toDay = By.name("toDay");
+        By economyClass = By.xpath("//input[@value='Coach']");
+        By businessClass = By.xpath("//input[@value='Business']");
+        By firstClass = By.xpath("//input[@value='First']");
+        By airline = By.name("airline");
+
+        browser.findElement(flights).click();
+
+        Thread.sleep(3000);
+        String type = "oneWay";
+        switch (type) {
+            case "roundTrip":
+                browser.findElement(roundtrip).click();
+            case "oneWay":
+                browser.findElement(oneway).click();
+        }
+
+        Select noOfPassengers = new Select(browser.findElement(passengers));
+        Select departingCountry = new Select(browser.findElement(departingFrom));
+        Select departingMonth = new Select(browser.findElement(fromMonth));
+        Select departingDay = new Select(browser.findElement(fromDay));
+        Select arrivingCountry = new Select(browser.findElement(arrivingIn));
+        Select arrivingMonth = new Select(browser.findElement(toMonth));
+        Select arrivingDay = new Select(browser.findElement(toDay));
+
+        noOfPassengers.selectByValue("2");
+        departingCountry.selectByValue("London");
+        departingMonth.selectByValue("12");
+        departingDay.selectByValue("21");
+        arrivingCountry.selectByValue("Paris");
+        arrivingMonth.selectByValue("12");
+        arrivingDay.selectByValue("22");
+
+        String serviceClass = "businessClass";
+        switch (serviceClass) {
+            case "economyClass":
+                browser.findElement(economyClass).click();
+            case "businessClass":
+                browser.findElement(businessClass).click();
+            case "firstClass":
+                browser.findElement(firstClass).click();
+        }
+
+        Select airlineName = new Select(browser.findElement(airline));
+        airlineName.selectByVisibleText("Unified Airlines");
     }
 
     @AfterClass
--- src/test/java/com/generic/autobot/testcases/VisaRegistration.java
+++ src/test/java/com/generic/autobot/testcases/VisaRegistration.java
@@ -2,19 +2,20 @@ package com.generic.autobot.testcases;
 
 import com.generic.autobot.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 import org.springframework.beans.factory.annotation.Value;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
+import static org.awaitility.Awaitility.await;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 public class VisaRegistration extends BaseTest {
-    By header = By.id("header_1");
-    By subHeader = By.id("subHeader_1");
-
     @Value("${visa.url}")
     private String visa;
 
@@ -28,13 +29,84 @@ public class VisaRegistration extends BaseTest {
     public void launch_application() {
         browser.manage().window().maximize();
         browser.navigate().to(visa);
-        assertThat(browser.findElement(header).getText().trim(), is(equalTo(heading)));
-        assertThat(browser.findElement(subHeader).getText().trim(), is(equalTo(subHeading)));
+
+        By header = By.id("header_1");
+        By subHeader = By.id("subHeader_1");
+
+        String pageHeader = browser.findElement(header).getText().trim();
+        String pageSubHeader = browser.findElement(subHeader).getText().trim();
+
+        assertThat(pageHeader, is(equalTo(heading)));
+        assertThat(pageSubHeader, is(equalTo(subHeading)));
     }
 
     @Test
-    public void visa_registration() {
-        System.out.println("Visa Registration");
+    public void tc_01_fill_user_details() {
+        By firstName = By.id("first_4");
+        By middleName = By.id("middle_4");
+        By lastName = By.id("last_4");
+
+        browser.findElement(firstName).sendKeys("Lionel");
+        browser.findElement(middleName).sendKeys("Andrés");
+        browser.findElement(lastName).sendKeys("Messi");
+    }
+
+    @Test
+    public void tc_02_fill_country_details() {
+        By fromCountry = By.id("input_46");
+        By toCountry = By.id("input_47");
+
+        Select fCountry = new Select(browser.findElement(fromCountry));
+        Select tCountry = new Select(browser.findElement(toCountry));
+
+        fCountry.selectByValue("Argentina");
+        tCountry.selectByValue("Spain");
+    }
+
+    @Test
+    public void tc_03_fill_birth_details() {
+        By month = By.id("input_24_month");
+        By day = By.id("input_24_day");
+        By year = By.id("input_24_year");
+
+        Select bMonth = new Select(browser.findElement(month));
+        Select bDay = new Select(browser.findElement(day));
+        Select bYear = new Select(browser.findElement(year));
+
+        bMonth.selectByValue("JUNE");
+        bDay.selectByValue("24");
+        bYear.selectByValue("1987");
+    }
+
+    @Test
+    public void tc_04_fill_contact_details() {
+        By userEmail = By.id("input_6");
+        By areaCode = By.id("input_27_area");
+        By phoneNumber = By.id("input_27_phone");
+
+        browser.findElement(userEmail).sendKeys("Lionel.Messi@gmail.com");
+        browser.findElement(areaCode).sendKeys("+54");
+        browser.findElement(phoneNumber).sendKeys("1234567890");
+    }
+
+    @Test
+    public void tc_05_submit_application() throws InterruptedException {
+        By comments = By.id("input_45");
+        By submit = By.id("submitBtn");
+
+        browser.findElement(comments).sendKeys("Lionel Messi Visa Form");
+        browser.findElement(submit).click();
+    }
+
+    @Test
+    public void tc_06_confirmation() {
+        By panel = By.id("panel");
+        By requestId = By.id("requestnumber");
+
+        await().atMost(Duration.ofSeconds(10))
+                .pollInterval(Duration.ofSeconds(1))
+                .until(() -> browser.findElement(requestId).isDisplayed() == true);
+        System.out.println("Confirmation Message:" + "\n" + browser.findElement(panel).getText());
     }
 
     @AfterClass
